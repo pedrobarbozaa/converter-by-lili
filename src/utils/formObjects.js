5e1e47cc1fb812298f202a7bb2c0b5fba7aeb328
unknown <pedrodeandrade2@gmail.com>
2025-07-19 17:51:17 -0300

    adicionando a estrutura do objeto e o esqueleto do formulário

---
+++ src/utils/formObjects.js
@@ -0,0 +1,12 @@
+const forms = {
+  circunference: 
+    { label: 'Raio', name: 'radius', type: 'number' },
+  radius: 
+    { label: 'Circunferência', name: 'circunference', type: 'number' },
+  clothSize: 
+    { label: 'Cintura', name: 'waist', type: 'number' },
+  hook: 
+    { label: 'Quadril', name: 'hip', type: 'number' },
+};
+
+export default forms
